bd517b7c3423b1e3bdbcd570c373c94d3b7ad01b
Ralph Plawetzki <ralph@purejava.org>
2022-06-16 16:05:59 +0200

    Add Menu

---
+++ src/main/java/org/purejava/Menu.java
@@ -0,0 +1,46 @@
+package org.purejava;
+
+import com.canonical.DBusMenu;
+import org.freedesktop.StatusNotifierItem;
+import org.freedesktop.StatusNotifierItemMenuItemListener;
+import org.freedesktop.dbus.exceptions.DBusException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Menu {
+    private Logger log = LoggerFactory.getLogger(Menu.class);
+
+    private static final String SERVICE_NAME = "org.purejava.testmenu";
+
+
+    private DBusMenu menu;
+    private List<StatusNotifierItem.MenuItem> entries;
+
+    public Menu() {
+        entries = new ArrayList<>();
+        entries.add(0, new StatusNotifierItem.MenuItem("Entry 1", new StatusNotifierItemMenuItemListener() {
+            @Override
+            public void onActivated() {
+
+            }
+        }));
+        entries.add(1, new StatusNotifierItem.MenuItem("Entry 2", new StatusNotifierItemMenuItemListener() {
+            @Override
+            public void onActivated() {
+
+            }
+        }));
+        try {
+            menu = new DBusMenu(SERVICE_NAME, entries);
+        } catch (DBusException e) {
+            log.error(e.toString(), e.getCause());
+        }
+    }
+
+    public DBusMenu getMenu() {
+        return menu;
+    }
+}
